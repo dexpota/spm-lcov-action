cf06201be50dd2a5994254911600a847dd470cb9
Fabrizio Destro <7031675+dexpota@users.noreply.github.com>
2023-02-10 11:13:53 +0100

    Add is-spm and test-binary arguments

--- cov.sh
+++ cov.sh
@@ -2,18 +2,6 @@
 
 PATH=$PATH
 
-
-# Find the test binary for SPM projects.
-BIN_PATH="$(swift build --show-bin-path)"
-XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
-COV_BIN=$XCTEST_PATH
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    f="$(basename $XCTEST_PATH .xctest)"
-    COV_BIN="${COV_BIN}/Contents/MacOS/$f"
-    PATH="/usr/local/opt/llvm/bin:$PATH"​
-fi
-
 INSTR_PROFILE=.build/debug/codecov/default.profdata
 IGNORE_FILENAME_REGEX=".build|Tests"
 
@@ -31,10 +19,29 @@ while :; do
         -i|--instr-profile) INSTR_PROFILE=$2
         shift
         ;;
+        -b|--test-binary) COV_BIN=$2
+        shift
+        ;;
+        --spm) IS_SPM=true
+        ;;
+        *) break
     esac
     shift
 done
 
+
+if [[ $IS_SPM == true  ]]; then
+    # Find the test binary for SPM projects.
+    BIN_PATH="$(swift build --show-bin-path)"
+    XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
+    COV_BIN=$XCTEST_PATH
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        f="$(basename $XCTEST_PATH .xctest)"
+        COV_BIN="${COV_BIN}/Contents/MacOS/$f"
+        PATH="/usr/local/opt/llvm/bin:$PATH"​
+    fi
+fi
 
 mkdir -p "$(dirname "$OUTPUT_FILE")"
 
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ async function main() {
     const format = core.getInput('file-format')
     const output = core.getInput('output-file')
     const instr_profile = core.getInput('instr-profile')
-    await exec.exec(`${__dirname}/cov.sh -f ${format} -o ${output} -i ${instr_profile}`)
+    const test_binary = core.getInput('test-binary')
+    const is_spm = core.getBooleanInput('is-spm') ? "--spm" : ""
+    await exec.exec(`${__dirname}/cov.sh -f ${format} -o ${output} -i ${instr_profile} -b ${test_binary} ${is_spm}`)
 }
 
 main().catch(err => core.setFailed(err.message))
