22eaa43eaeb38125dc706be6971df91db3e1670d
Fabrizio Destro <destro.fabrizio@gmail.com>
2025-02-11 18:10:16 +0100

    Add CommandBuilder

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const core = require('@actions/core')
 const exec = require('@actions/exec')
 
+class CommandBuilder {
+    constructor(baseCommand) {
+      this.baseCommand = baseCommand;
+      this.args = [];
+    }
+
+    addFlag(flag, value) {
+      if (value) {
+        this.args.push(`${flag} ${value}`);
+      }
+      return this;
+    }
+
+    addBooleanFlag(flag, condition) {
+      if (condition) {
+        this.args.push(flag);
+      }
+      return this;
+    }
+
+    build() {
+      return `${this.baseCommand} ${this.args.join(' ')}`.trim();
+    }
+}
+
 async function main() {
-    const format = core.getInput('file-format')
-    const output = core.getInput('output-file')
-    const instr_profile = core.getInput('instr-profile')
-    const test_binary = core.getInput('test-binary')
-    const is_spm = core.getBooleanInput('is-spm') ? "--spm" : ""
-    await exec.exec(`${__dirname}/cov.sh -f ${format} -o ${output} -i ${instr_profile} -b ${test_binary} ${is_spm}`)
+    const format = core.getInput('file-format');
+    const output = core.getInput('output-file');
+    const instr_profile = core.getInput('instr-profile');
+    const test_binary = core.getInput('test-binary');
+    const is_spm = core.getBooleanInput('is-spm');
+
+    const command = new CommandBuilder(`${__dirname}/cov.sh`)
+        .addFlag('-f', format)
+        .addFlag('-o', output)
+        .addFlag('-i', instr_profile)
+        .addFlag('-b', test_binary)
+        .addBooleanFlag('--spm', is_spm)
+        .build();
+
+    await exec.exec(command);
 }
 
 main().catch(err => core.setFailed(err.message))
